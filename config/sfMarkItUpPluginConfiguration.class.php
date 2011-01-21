a64dd8f3bc0406f96106f5ec33ad2b3a94f28838
Tomasz Zdanowski <tomasz@amilo.(none)>
2011-01-21 13:06:57 +0100

    first import

---
+++ config/sfMarkItUpPluginConfiguration.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * sfMarkItUpPlugin configuration.
+ * 
+ * @package     sfMarkItUpPlugin
+ * @subpackage  config
+ * @author      Your name here
+ * @version     SVN: $Id: PluginConfiguration.class.php 17207 2009-04-10 15:36:26Z Kris.Wallsmith $
+ */
+class sfMarkItUpPluginConfiguration extends sfPluginConfiguration
+{
+  const VERSION = '1.0.0-DEV';
+
+  /**
+   * @see sfPluginConfiguration
+   */
+  public function initialize()
+  {
+  }
+}
